aa7d90741b805400271e2bb49c949bb18d06ec33
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-06-05 15:31:44 +0700

    Change fetching url

--- src/services/Const.js
+++ src/services/Const.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "http://localhost:8080";
-const BASE_URL = "http://haui-megatech.shop";
+const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://haui-megatech.shop";
 
 export default BASE_URL;
